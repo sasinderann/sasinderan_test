53d8af7a048af969d1784b82c88dcf90907020dc
Sasinderan <sasinderann@gmail.com>
2025-12-23 23:57:39 +0530

    added investment consolidate

--- sasinderan_demo/Model/HoldingsModel.swift
+++ sasinderan_demo/Model/HoldingsModel.swift
@@ -10,24 +10,3 @@ import Foundation
 struct HoldingsModel: Codable {
     var data: UserHoldings?
 }
-
-struct UserHoldings: Codable {
-    var userHolding: [IndividualHolding]?
-}
-
-struct IndividualHolding: Codable {
-    var symbol: String?
-    var quantity: Int?
-    var ltp: Double?
-    var avgPrice: Double?
-    var close: Double?
-    
-    func getCurrentValue() -> Double {
-        if let quantity = self.quantity, let lastTradePrice = self.ltp, let avgPrice = self.avgPrice {
-            let currentValue = lastTradePrice * Double(quantity)
-            let invested = avgPrice * Double(quantity)
-            return currentValue - invested
-        }
-        return -1
-    }
-}
---
+++ sasinderan_demo/Model/UserHoldings.swift
@@ -0,0 +1,44 @@
+//
+//  UserHoldings.swift
+//  sasinderan_demo
+//
+//  Created by SASINDERAN N on 23/12/25.
+//
+
+import Foundation
+
+struct UserHoldings: Codable {
+    var userHolding: [IndividualHolding]?
+    
+    func getInvestmentConsolidate() -> InvestmentConsolidate {
+        let totalLossAndProfit = getCurrentValue() - getInitialInvestment()
+        return InvestmentConsolidate(currentValue: getCurrentValue(), totalInvestment: getInitialInvestment(), todayPnL: getTodayProfitAndLoss(), totalPnL: totalLossAndProfit)
+    }
+    
+    func getInitialInvestment() -> Double {
+        if let allHoldings = self.userHolding {
+            let value = allHoldings.map { Double($0.quantity.zeroUnWrapped) * $0.avgPrice.zeroUnWrapped }
+            return value.reduce(0, +)
+        } else {
+            return -1
+        }
+    }
+    
+    func getCurrentValue() -> Double {
+        if let allHoldings = self.userHolding {
+            let value = allHoldings.map { Double($0.quantity.zeroUnWrapped) * $0.ltp.zeroUnWrapped }
+            return value.reduce(0, +)
+        } else {
+            return -1
+        }
+    }
+    
+    func getTodayProfitAndLoss() -> Double {
+        if let allHoldings = self.userHolding {
+            let value = allHoldings.map { Double($0.quantity.zeroUnWrapped) * ($0.close.zeroUnWrapped - $0.ltp.zeroUnWrapped) }
+            return value.reduce(0, +)
+        } else {
+            return -1
+        }
+    }
+}
